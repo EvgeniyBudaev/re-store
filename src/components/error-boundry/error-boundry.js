f7e4456fc58ea3850630e72ac11ce3a4bf24d436
EvgeniyBudaev <budaev.e@gmail.com>
2020-01-19 22:10:30 +0300

    fix error-boundry

--- src/components/error-boundry/error-boundry.js
+++ src/components/error-boundry/error-boundry.js
@@ -1,6 +1,6 @@
-import React, { component, Component } from 'react';
+import React, { Component } from "react";
 
-import ErrorIndicator from '../error-indicator';
+import ErrorIndicator from "../error-indicator";
 
 export default class ErrorBoundry extends Component {
   state = {
@@ -9,13 +9,13 @@ export default class ErrorBoundry extends Component {
 
   componentDidCatch() {
     this.setState({ hasError: true });
-  };
+  }
 
   render() {
     if (this.state.hasError) {
-      return <ErrorIndicator />
+      return <ErrorIndicator />;
     }
 
     return this.props.children;
   }
-};
+}
