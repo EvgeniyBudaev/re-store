3ab516fdce5782ad74c34f5bd37b22e758e41ee3
EvgeniyBudaev <budaev.e@gmail.com>
2020-01-20 19:06:11 +0300

    action dispatch
    136. Отправка действий в Redux Store
    (action dispatch)

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -2,21 +2,8 @@ import React from "react";
 import BookList from '../book-list';
 
 const HomePage = () => {
-  const books = [
-    {
-      id: 1,
-      title: "Learn React",
-      author: "EasyUm"
-    },
-    {
-      id: 2,
-      title: "Learn Redux",
-      author: "Udemy"
-    }
-  ];
-
   return (
-    <BookList books={books} />
+    <BookList />
   )
 };
 
